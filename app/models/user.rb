dc68fd68d0c59be09ad6314c9cb8bb62c95503c6
Rutuja Nirmal <64598190+rutujanirmal@users.noreply.github.com>
2022-09-19 19:19:01 +0530

    Model/user using devise-token_auth (#1)
    * Add devise_token_auth for authentication
    
    * Create User Model
    
    * Automodify application_controller.rb

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class User < ActiveRecord::Base
+  extend Devise::Models
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  include DeviseTokenAuth::Concerns::User
+
+  enum role: {
+    "student": 0,
+    "teacher":1
+  }
+
+end
